75a38b2efb4af280fdf47718ee7917717581f72e
Rob Hess <hessro@oregonstate.edu>
2022-01-27 15:52:28 -0800

    Add code from 1/27 lecture.

--- app/src/main/java/com/example/android/githubsearch/MainActivity.kt
+++ app/src/main/java/com/example/android/githubsearch/MainActivity.kt
@@ -3,32 +3,31 @@ package com.example.android.githubsearch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.JsonAdapter
 import com.example.android.githubsearch.data.GitHubRepo
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class MainActivity : AppCompatActivity() {
+    private val apiBaseUrl = "https://api.github.com"
+    private val tag = "MainActivity"
+
+    private lateinit var requestQueue: RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dummySearchResults = listOf<GitHubRepo>(
-            GitHubRepo("Dummy search results"),
-            GitHubRepo("Dummy search results"),
-            GitHubRepo("Dummy search results"),
-            GitHubRepo("Dummy search results"),
-            GitHubRepo("Dummy search results"),
-            GitHubRepo("Dummy search results"),
-            GitHubRepo("Dummy search results"),
-            GitHubRepo("Dummy search results"),
-            GitHubRepo("Dummy search results"),
-            GitHubRepo("Dummy search results"),
-            GitHubRepo("Dummy search results"),
-            GitHubRepo("Dummy search results")
-        )
+        requestQueue = Volley.newRequestQueue(this)
 
         val searchBoxET: EditText = findViewById(R.id.et_search_box)
         val searchBtn: Button = findViewById(R.id.btn_search)
@@ -43,9 +42,36 @@ class MainActivity : AppCompatActivity() {
         searchBtn.setOnClickListener {
             val query = searchBoxET.text.toString()
             if (!TextUtils.isEmpty(query)) {
-                adapter.updateRepoList(dummySearchResults)
+                doRepoSearch(query)
                 searchResultsListRV.scrollToPosition(0)
             }
         }
     }
+
+    fun doRepoSearch(q: String, sort: String = "stars") {
+        val url = "$apiBaseUrl/search/repositories?q=$q&sort=$sort"
+
+        val moshi = Moshi.Builder()
+            .addLast(KotlinJsonAdapterFactory())
+            .build()
+        val jsonAdapter: JsonAdapter<GitHubSearchResults> =
+            moshi.adapter(GitHubSearchResults::class.java)
+
+        val req = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                Log.d(tag, it)
+            },
+            {
+                Log.d(tag, "Error fetching from $url: ${it.message}")
+            }
+        )
+
+        requestQueue.add(req)
+    }
+
+    private data class GitHubSearchResults(
+        val items: List<GitHubRepo>
+    )
 }
--- app/src/main/java/com/example/android/githubsearch/data/GitHubRepo.kt
+++ app/src/main/java/com/example/android/githubsearch/data/GitHubRepo.kt
@@ -1,3 +1,10 @@
 package com.example.android.githubsearch.data
 
-data class GitHubRepo(val name: String)
+import com.squareup.moshi.Json
+
+data class GitHubRepo(
+    @Json(name = "full_name") val name: String,
+    val description: String,
+    @Json(name = "html_url") val url: String,
+    @Json(name = "stargazers_count") val stars: Int
+)
